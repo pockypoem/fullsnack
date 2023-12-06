16afe0b2ac463b98d82495404437f2fc5eac9259
pockypoem <jeremyas.cornelis@gmail.com>
2023-12-06 14:40:16 +0700

    feat: homepage

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ const App = () => {
                     </div>
                 )}
                 <div className={
-
                     `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? ' md:ml-72' : 'flex-2'}`
                 }>
                     
@@ -43,37 +42,37 @@ const App = () => {
                         <Navbar />
                     </div>
 
+                
+
+                    <div>
+                        <Routes>
+                            {/* Dashboard */}
+                            <Route path='/' element={<Ecommerce />} />
+                            <Route path='/ecommerce' element={<Ecommerce />} />
+
+                            {/* Pages */}
+                            <Route path='/orders' element={<Orders />} />
+                            <Route path='/employees' element={<Employees />} />
+                            <Route path='/customers' element={<Customers />} />
+
+                            {/* Apps */}
+                            <Route path='/kanban' element={<Kanban />} />
+                            <Route path='/editor' element={<Editor />} />
+                            <Route path='/calendar' element={<Calendar />} />
+                            <Route path='/color-picker' element={<ColorPicker />} />
+
+                            {/* Charts */}
+                            <Route path='/line' element={<Line />} />
+                            <Route path='/area' element={<Area />} />
+                            <Route path='/bar' element={<Bar />} />
+                            <Route path='/pie' element={<Pie />} />
+                            <Route path='/financial' element={<Financial />} />
+                            <Route path='/color-mapping' element={<ColorMapping />} />
+                            <Route path='/pyramid' element={<Pyramid />} />
+                            <Route path='/stacked' element={<Stacked />} />
+                        </Routes>
+                    </div>
                 </div>
-
-                <div>
-                    <Routes>
-                        {/* Dashboard */}
-                        <Route path='/' element={<Ecommerce />} />
-                        <Route path='/ecommerce' element={<Ecommerce />} />
-
-                        {/* Pages */}
-                        <Route path='/orders' element={<Orders />} />
-                        <Route path='/employees' element={<Employees />} />
-                        <Route path='/customers' element={<Customers />} />
-
-                        {/* Apps */}
-                        <Route path='/kanban' element={<Kanban />} />
-                        <Route path='/editor' element={<Editor />} />
-                        <Route path='/calendar' element={<Calendar />} />
-                        <Route path='/color-picker' element={<ColorPicker />} />
-
-                        {/* Charts */}
-                        <Route path='/line' element={<Line />} />
-                        <Route path='/area' element={<Area />} />
-                        <Route path='/bar' element={<Bar />} />
-                        <Route path='/pie' element={<Pie />} />
-                        <Route path='/financial' element={<Financial />} />
-                        <Route path='/color-mapping' element={<ColorMapping />} />
-                        <Route path='/pyramid' element={<Pyramid />} />
-                        <Route path='/stacked' element={<Stacked />} />
-                    </Routes>
-                </div>
-
             </div>
         </BrowserRouter>
         </div>
